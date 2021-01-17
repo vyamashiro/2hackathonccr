7eb4ae5cbd01426e38cc3535393329a6bf63dba3
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-01-16 22:44:29 -0300

    pontuação atualizada

--- aceitarAtividade.js
+++ aceitarAtividade.js
@@ -1,5 +1,5 @@
-function aceitarAtividade() {
-    pontosAtividade = 35
+function aceitarAtividade(id) {
+    pontosAtividade = parseInt(id)
     pontuacaoAtual = localStorage.getItem('pontuação')
     novaPontuacao = pontuacaoAtual != null ? parseInt(pontuacaoAtual) + parseInt(pontosAtividade) : pontuacaoAtual = pontosAtividade
     localStorage.setItem('pontuação', novaPontuacao)
