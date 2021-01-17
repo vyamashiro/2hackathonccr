97b46e610cb73431efc453847c8dcf7012425196
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-01-16 21:14:45 -0300

    adicionando modal para todos os cards

--- aceitarAtividade.js
+++ aceitarAtividade.js
@@ -1,6 +1,7 @@
 function aceitarAtividade() {
-    pontosAtividade = 10
+    pontosAtividade = 35
     pontuacaoAtual = localStorage.getItem('pontuação')
     novaPontuacao = pontuacaoAtual != null ? parseInt(pontuacaoAtual) + parseInt(pontosAtividade) : pontuacaoAtual = pontosAtividade
     localStorage.setItem('pontuação', novaPontuacao)
+    $('#modalExemplo').modal('hide');
 }
